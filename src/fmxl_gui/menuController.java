093a1a19b3e6e6a2d52a68e6017d4899f1148d99
kaspergrispino <kaspergrispino@kaspers-mbp.home>
2020-03-24 22:43:26 -0400

    Billing functionality added

--- src/fmxl_gui/menuController.java
+++ src/fmxl_gui/menuController.java
@@ -7,12 +7,11 @@ import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
@@ -21,40 +20,23 @@ import javafx.util.Duration;
 
 public class menuController implements Initializable {
 	
+	ObservableList<String> order = FXCollections.observableArrayList();
+	
 	@FXML
-    private void handleOnButtonAction(ActionEvent event)
+    private ListView<String> userOrder;
+	
+	@FXML
+    private void handleOnButtonAction(MouseEvent event)
     {
-        System.out.println("You clicked button: " + ((Button)event.getSource()).getId());
-    }
-
-    @FXML private void hamburger_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
-    }
-    
-    @FXML private void chicken_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
-    }
-    
-    @FXML private void fries_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
-    }
-    
-    @FXML private void hotdog_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
-    }
-    
-    @FXML private void shackburger_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
-    }
-    
-    @FXML private void smokeshack_btn_MouseClicked(MouseEvent event)
-    {
-        System.out.println("You clicked rectangle: " + ((Rectangle)event.getSource()).getId());
+        System.out.println("You clicked button: " + ((Rectangle)event.getSource()).getId());
+        order.add(((Rectangle)event.getSource()).getId());
+        userOrder.setItems(order);
+        double cost = calCost();
+        double tax = calTax(calCost());
+        double total = cost + tax;
+        orderCost.setText("Cost:	" + String.format("%.2f", cost));
+        orderTax.setText("Tax:	" + String.format("%.2f", tax));
+        totalOrdercost.setText("Total:	" + String.format("%.2f", total));
     }
     
     @FXML
@@ -73,15 +55,83 @@ public class menuController implements Initializable {
     @FXML
     private ListView<String> locationList;
     
+    //https://stackoverflow.com/questions/40964403/filling-listview-from-fxml-file-in-javafx-empty-listview
+    private void initLocations(ObservableList<String> elements) {
+    	elements.add("Location 1");
+        elements.add("Location 2");
+        elements.add("Location 3");
+        locationList.setItems(elements);
+    }
+    
+    @FXML
+    private Label selectedLocation;
+    
+    @FXML
+    private Label orderCost;
+    
+    private double calCost() {
+    	//https://stackoverflow.com/questions/31262040/how-can-i-edit-a-specific-object-in-observablelist
+    	
+    	int order_length = order.size();
+    	double cost = 0;
+    	for (int i = 0; i < order_length; i++) {
+    		if("hamburger".equals(order.get(i))){
+    			cost = cost + 3.49;
+    		}
+    		else if("chicken".equals(order.get(i))){
+    			cost = cost + 3.49;
+    		}
+    		else if("fries".equals(order.get(i))){
+    			cost = cost + 1;
+      		}
+    		else if("hotdog".equals(order.get(i))){
+    			cost = cost + 3.49;
+    		}
+    		else if("shackburger".equals(order.get(i))){
+    			cost = cost + 3.49;
+      		}
+    		else if("smokeshack".equals(order.get(i))){
+    			cost = cost + 3.49;
+    		}
+    	}  	
+    	return cost;
+    }
+    
+    @FXML
+    private Label orderTax;
+    
+    private double calTax(double cost) {
+    	double tax = cost * .08875;
+    	return tax;
+    	
+    }
+    
+    @FXML
+    private Label totalOrdercost;
+    
+    private double calTotal(double cost, double tax) {
+    	double total = cost + tax;
+    	return total;
+    	
+    }
+    
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initClock();
         
         ObservableList<String> elements = FXCollections.observableArrayList();
-        elements.add("first");
-        elements.add("second");
-        elements.add("third");
-        locationList.setItems(elements);
+        initLocations(elements);
+        
+       //Update label in corner with selected location
+       //https://self-learning-java-tutorial.blogspot.com/2018/06/javafx-listview-get-selected-item.html
+        locationList.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
+            String selectedItem = locationList.getSelectionModel().getSelectedItem();
+            //int index = locationList.getSelectionModel().getSelectedIndex();
+            
+            selectedLocation.setText("Location Selected: " + selectedItem);
+           });
+        
     }
 
 }
